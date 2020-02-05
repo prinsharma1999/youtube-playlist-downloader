ec8adfc190a1422626e23f292f84fb5d7c5b33f1
prin sharma <48044355+prinsharma1999@users.noreply.github.com>
2020-02-05 14:12:43 +0530

    added package check function

--- ypd.sh
+++ ypd.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#to check package is installed or not without distribution dependency
+pkg=youtube-dl
+which $pkg > /dev/null 2>&1
+if [ $? == 0 ]
+then
+echo "type 1 for video : type 2 for playlist :"
+read -p "enter here : " u
+else
+read -p "$pkg is not installed. Answer Y/N if want installation_ " request
+if  [ $request == "Y" ]
+then
+apt-get install $pkg
+fi
+fi
+#to download video using youtube-dl
 read -p "enter the folder name : " fol
 mkdir $fol
 cd $fol
@@ -6,16 +21,12 @@ n=youtube-dl
 m=-cit
 o=-f
 p=best
-echo "type 1 for video : type 2 for playlist :"
-#echo " :"
-read -p "enter here : " u
 read -p "hello put your link here : " url
 if [ "$u" = "1" ];
 then
 	$n $o $p $url
+else [ "$u" = "2" ];
+$n $m $url
 fi
-if [ "$u" = "2" ];
-then
-	$n $m $url
-fi
-
+ls > list.txt
+#there are still need to be changed a lot of , so make changes if you can 
